9ed06457fa0299773c6b46876526d72694b6d523
tyrion.lv <tyrion.lv@perficient.com>
2017-12-14 16:43:23 +0800

    Test Chrome DevTools function "js/css coverage"

---
+++ Vue Demo/js/testChromDevTools.js
@@ -0,0 +1,8 @@
+/* Test Chrome DevTools function: Performance 'instrumenting coverage' */
+function testInstrumentingCoverage() {
+    console.log('Test instrumenting coverage');
+    console.log('A new gesture for Continue to Here');
+    alert('Test instrumenting coverage');
+    console.log('Hello');
+    console.log('World!');
+}
